7831ccb169a150de7712c470f34264d2e9fece58
nyawnayaw <hglenaya@gmail.com>
2023-12-22 06:46:26 +0700

    update tugas 9

---
+++ app/Views/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-color: #f5f5f5;
+        }
+
+        .container {
+            text-align: center;
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333333;
+        }
+
+        p {
+            color: #666666;
+            margin-top: 10px;
+        }
+    </style>
+    <title>Toko Online</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome Home!</h1>
+        <p>Welcome to our awesome website.</p>
+    </div>
+</body>
+</html>
